de6bafa0274cf879ee21772d7c8c4c17d0d7c3e7
clareacollins <119975593+clareacollins@users.noreply.github.com>
2024-05-30 17:22:30 -0400

    Update __main__.py

--- sniper/__main__.py
+++ sniper/__main__.py
@@ -1,25 +1,117 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-import sys
-import files, dir
-import tumblr, patreon, other
+from selenium.webdriver.common.by import By
 
-dest = dir.root + "\\in"
-target_file = dir.root + "\\target1.txt"
+from sniper import tumblr, patreon, twitter, other, login, scope
+
+import dir
+
+chrome_options = Options()
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--log-level=3')
 
 if __name__ == "__main__":
+    print("Welcome to the main Sniper Shell!")
+    print("Please wait while the driver is established...")
 # Establish Driver
-    driver = webdriver.Chrome()
-    driver.set_window_size(1024, 1940)
-# Grab Targets from File
-    targets = files.readLines(target_file)
-    print(f"Sniper Engaged: {len(targets)} Targets Aquired")
-    if sys.argv[1] == "tumblr":
-        tumblr.main(driver, targets, sys.argv[2])
-    elif sys.argv[1] == "patreon":
-        patreon.main(driver, targets)
-    elif sys.argv[1] == "other":
-        other.main(driver, targets, sys.argv[2])
+    driver = webdriver.Chrome(options=chrome_options)
+    print("Driver Established")
+    print("Enter a Platform:\n-> tumblr\n-> patreon\n-> twitter\n-> other")
+    while True:
+        # break
+        platform = input("> ")
+        if platform == "exit":
+            break
+        elif platform == "tumblr":
+            print("Logging into Tumblr...")
+            login.tumblrLogin(driver)
+            print("Welcome to the Tumblr Shell")
+            print("Commands: check, ingest, cap, prev, pic, in, data, source, text, reblog, test, link, url, archive, feed")
+            while True:
+                command = input("tumblr> ")
+                if command == "exit":
+                    print("Exiting the Tumblr Shell")
+                    break
+                elif command == "help":
+                    print("Commands:" + \
+                        "\n\tcheck - open url and pause" + \
+                        "\n\tingest - grab pics and capture" + \
+                        "\n\tcap - capture post" + \
+                        "\n\tprev - capture top of post" + \
+                        "\n\tpic - grab pics" + \
+                        "\n\tin - assemble post with pics inline" + \
+                        "\n\tdata - get post data" + \
+                        "\n\tsource - get source data" + \
+                        "\n\ttext - get all text" + \
+                        "\n\treblog - get reblog count" + \
+                        "\n\ttest - get lost bool" + \
+                        "\n\tlink - get links in post text" + \
+                        "\n\turl - get blog-first url" + \
+                        "\n\tarchive - get links from users in archive" + \
+                        "\n\tfeed - get links from users with no archive page" + \
+                        "\n\thelp - display this message" + \
+                        "\n\texit - exit the tumblr shell")
+                else:
+                    tumblr.main(driver, command)
+        elif platform == "patreon":
+            print("Logging into Patreon...")
+            login.patreonLogin(driver)
+            print("Welcome to the Patreon Shell")
+            while True:
+                command = input("patreon> ")
+                if command == "exit":
+                    print("Exiting Patreon Shell")
+                    break
+                elif command == "help":
+                    print("Commands:" + \
+                        "\n\tcheck - open url and pause" + \
+                        "\n\tingest - grab pics and capture" + \
+                        "\n\tcap - capture post" + \
+                        "\n\tprev - capture top of post" + \
+                        "\n\tpic - grab pics" + \
+                        "\n\tdata - get post data" + \
+                        "\n\ttext - get all text" + \
+                        "\n\tfeed - get links from users in feed" + \
+                        "\n\thelp - display this message" + \
+                        "\n\texit - exit the patreon shell")
+                else:
+                    patreon.main(driver, command)
+        elif platform == "other":
+            print("Welcome to the Other Shell")
+            while True:
+                command = input("other> ")
+                if command == "exit":
+                    print("Exiting Generic Shell")
+                    break
+                elif command == "help":
+                    print("Commands:" + \
+                        "\n\texit - exit the other shell")
+                else:
+                    other.main(driver, command)
+        elif platform == "test":
+            driver.set_window_size(1024, 1940)
+            print("Welcome to the Test Shell")
+            while True:
+                command = input("test> ")
+                if command == "exit":
+                    print("Exiting Test Shell")
+                    break
+                elif command == "help":
+                    print("Commands:" + \
+                        "\n\texit - exit the test shell")
+                else:
+                    try:
+                        element = driver.find_element(By.CSS_SELECTOR, command)
+                        print(element)
+                        scope.captureElement(driver, f"{dir.root}\\in\\preview\\test - {command}", element)
+                    except:
+                        print("Element not found")
+                    
+        else:
+            print("Invalid Platform. Please try again or type 'exit' to exit the shell.")
+    
+    print("Exiting the Sniper Shell")
     driver.quit()
     print("Misson Complete")
     
